02c50346d4f74266b6b0ae7c7db10f6ed5c2ddc6
Tucker Blackwell <tuckerblackwell@higharc.com>
2025-09-04 19:38:41 -0600

    Initial Commit

---
+++ Seeker/SeekerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekerApp.swift
+//  Seeker
+//
+//  Created by Tucker Reeve Blackwell on 9/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SeekerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SeekerTests/SeekerTests.swift
@@ -0,0 +1,17 @@
+//
+//  SeekerTests.swift
+//  SeekerTests
+//
+//  Created by Tucker Reeve Blackwell on 9/4/25.
+//
+
+import Testing
+@testable import Seeker
+
+struct SeekerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
